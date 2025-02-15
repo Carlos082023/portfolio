24bdc1503a4983f3be760a109093e4cacf41dd7a
Carlos Vladislavic <Carlos082023@users.noreply.github.com>
2025-02-15 17:22:51 -0300

    Mejora del css y el responsive y el menu hamburguesa

--- javascrip.js
+++ javascrip.js
@@ -13,3 +13,29 @@ function MyName() {
 }
 
 document.addEventListener('DOMContentLoaded', MyName);
+
+// Capturar el botón y el menú
+document.addEventListener("DOMContentLoaded", () => {
+    const menuIcon = document.getElementById("menu-icon");
+    const navList = document.getElementById("navlist");
+
+    menuIcon.addEventListener("click", () => {
+        navList.classList.toggle("show");
+    });
+});
+
+const sr = ScrollReveal({
+    distance : '64px',
+    duration :2000,
+    delay : 450,
+    reset :true
+})
+
+
+sr.reveal('.hero-text', {delay: 200, origin: 'top'})
+sr.reveal('.hero-img', {delay: 450, origin: 'top'})
+sr.reveal('.icons', {delay: 500, origin: 'left'})
+sr.reveal('.scroll-down', {delay: 450, origin: 'right'})
+
+
+
